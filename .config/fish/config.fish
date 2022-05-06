ff7922d539169301aa8302722271fc0912cbfc1c
André Gonzalez <lopescg@gmail.com>
2022-05-05 23:19:32 -0300

    changed the indicators of the vim mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,24 @@ function fish_user_key_bindings
         bind -M $mode \cf forward-char
     end
 end
+function fish_mode_prompt
+  switch $fish_bind_mode
+    case default
+      set_color --bold blue
+      echo '|n| '
+    case insert
+      set_color normal
+      echo '|i| '
+    case replace_one
+      set_color --bold green
+      echo '|R| '
+    case visual
+      set_color --bold brmagenta
+      echo '|V| '
+    case '*'
+      set_color --bold red
+      echo '|?| '
+  end
+  set_color normal
+end
 end
