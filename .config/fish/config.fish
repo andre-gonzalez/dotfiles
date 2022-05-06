d4099012b116f036a4aa21eea3694478164e6b71
André Gonzalez <lopescg@gmail.com>
2022-05-06 01:41:47 -0300

    Added comments to the config file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+# Bring the aliases from bash
 source ~/.bash_aliases
+
+# Enable vi mode
 fish_vi_key_bindings
+
+# Allow ctrl + f to autocomplete to work in vi mode
 function fish_user_key_bindings
     for mode in insert default visual
         bind -M $mode \cf forward-char
     end
 end
+
+# Change the vim prompt 
 function fish_mode_prompt
   switch $fish_bind_mode
     case default
