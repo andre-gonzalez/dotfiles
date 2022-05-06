6ae9234583652504b9daa003a091be39e23cade5
André Gonzalez <lopescg@gmail.com>
2022-05-05 22:56:24 -0300

    added ctrl f to complete in vim mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 source ~/.bash_aliases
 fish_vi_key_bindings
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \cf forward-char
+    end
+end
 end
