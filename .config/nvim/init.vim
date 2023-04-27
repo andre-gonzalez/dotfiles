806386621c1f8cc36a3ab1b335592ec4a29fb39e
André Gonzalez <lopescg@gmail.com>
2023-04-26 20:03:19 -0600

    continuing transition to init.lua

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,23 +1,6 @@
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-" Basics
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-		source ~/.config/nvim/transition.lua
-
-		"alternate between relative line numbers and absolute based in wich mode you are
-		" set number
-		augroup numbertoggle
-		  autocmd!
-		  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
-		  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
-		augroup END
-
-		" auto-format
-		autocmd BufWritePre *.py lua vim.lsp.buf.formatting_sync(nil, 100)
-		" autocmd BufWritePre *.sql lua vim.lsp.buf.formatting_sync(nil, 100)
-
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 "Plugins
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 		call plug#begin()
 
 				"Set a infomative line in the bottom of the editor
@@ -112,69 +95,8 @@
 				Plug 'Exafunction/codeium.vim'
 		call plug#end()
 
-
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-" Plugins configuration
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-
-		"firenvim config
-		let g:firenvim_config = {
-			\ 'globalSettings': {
-				\ 'alt': 'all',
-				\ 'takeover': 'never'
-			\  },
-			\ 'localSettings': {
-				\ '.*': {
-				\ },
-			\ }
-		\ }
-		let fc = g:firenvim_config['localSettings']
-		let fc['https?://.+'] = { 'takeover': 'never', 'priority': 1 }
-		let fc['.*'] = { 'cmdline' : 'neovim' }
-		au BufEnter d8c7fc-e847.cloud.databricks.com_*.txt set filetype=sql
-		au BufEnter mail.google.com_*.txt set filetype=markdown
-		let g:timer_started = v:false
-
-		"vim-slime
-		let g:slime_target = "tmux"
-		let g:slime_paste_file = expand("$HOME/.cache/slime_paste")
-
-		"Vim airline
-		let g:airline#extensions#tabline#enabled = 0 " Enable the list of buffers
-		let g:airline#extensions#tabline#formatter = 'default'  " f/p/file-name.js
-		let g:airline#extensions#tabline#formatter = 'jsformatter' " path-to/f
-		let g:airline#extensions#tabline#formatter = 'unique_tail' " file-name.js
-		let g:airline#extensions#tabline#formatter = 'unique_tail_improved' " f/p/file-name.js
-		let g:airline#extensions#disable_rtp_load=1 "fix error messages when starting neovim
-
-		"Database operations
-		let g:db_ui_save_location = '~/eureciclo/salesops-dw/main-salesops-dw/queries'
-		let g:db_ui_tmp_query_location = '~/eureciclo/salesops-dw/main-salesops-dw/tmp'
-		let g:db_ui_winwidth = 10 " change drawer width
-		let g:db_ui_execute_on_save = 0 " Disable the executing of the query on save
-		let g:db_ui_force_echo_notifications = 1 "Force notifications to be show on command line
-		autocmd FileType sql setlocal omnifunc=vim_dadbod_completion#omni
-
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-" Remaps
+" INIT.LUA
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-
-		" remap to run telescope with \ + f
-		nnoremap <Leader>f :lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<CR>
-		nnoremap <leader>g :lua require('telescope.builtin').live_grep({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<CR>
-		nnoremap <leader>c :Telescope file_browser<CR>
-
-		"autoformat SQL
-		" autocmd FileType sql call SqlFormatter()
-		" augroup end
-		" function SqlFormatter()
-		" 	set noai
-		" 	" set mappings...
-		" 	map <leader>pt  :%!sqlformat --reindent --keywords upper --identifiers lower -<CR>
-		" endfunction
-
-		"SQL Server completion
-		let g:LanguageClient_serverCommands = {
-			\ 'sql': ['sql-language-server', 'up', '--method', 'stdio'],
-			\ }
+		source ~/.config/nvim/transition.lua
--- .config/nvim/transition.lua
+++ .config/nvim/transition.lua
@@ -108,6 +108,7 @@ vim.opt.cmdheight = 0
 --Disable spellchecker
 vim.opt.spell = false
 
+
 ------------------------------------------------------------------------------
 ----------------------------- REMAPS -----------------------------------------
 ------------------------------------------------------------------------------
@@ -116,6 +117,8 @@ vim.opt.spell = false
 -- vim.keymap.set('n', '<C-L>', ':nohlsearch<CR>')
 
 -- Remaped the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- DBUI
 vim.keymap.set('n', '<leader>bf', ':DBUIFindBuffer<CR>')
@@ -226,3 +229,79 @@ vim.cmd('ab SDIST SELECT DISTINCT<cr>FROM<cr>;<up><right><right><right>')
 vim.cmd('ab SCOLUMNS SELECT "column_name"<cr>FROM information_schema."columns"<cr>WHERE "table_name" = \'\'<cr>;<up><right><right><right><right><right><right><right><right><right><right><right><right><right><right><right><right><right><right><right><right><right>')
 vim.cmd('ab skeletonmd #<cr><cr>## Participantes:<cr>- [[]]<cr><cr>---<cr># Notas:<cr>- <cr>---<cr># To-do:<cr>- [ ]')
 vim.cmd('ab dcom # COMMAND ----------')
+
+------------------------------------------------------------------------------
+----------------------------- OLD VIM COMMANDS -------------------------------
+------------------------------------------------------------------------------
+vim.cmd([[
+"alternate between relative line numbers and absolute based in wich mode you are
+augroup numbertoggle
+  autocmd!
+  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+augroup END
+
+" auto-format
+autocmd BufWritePre *.py lua vim.lsp.buf.formatting_sync(nil, 100)
+" autocmd BufWritePre *.sql lua vim.lsp.buf.formatting_sync(nil, 100)
+
+nnoremap <Leader>f :lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<CR>
+nnoremap <leader>g :lua require('telescope.builtin').live_grep({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<CR>
+nnoremap <leader>c :Telescope file_browser<CR>
+
+"autoformat SQL
+" autocmd FileType sql call SqlFormatter()
+" augroup end
+" function SqlFormatter()
+" 	set noai
+" 	" set mappings...
+" 	map <leader>pt  :%!sqlformat --reindent --keywords upper --identifiers lower -<CR>
+" endfunction
+
+"SQL Server completion
+let g:LanguageClient_serverCommands = {
+	\ 'sql': ['sql-language-server', 'up', '--method', 'stdio'],
+	\ }
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+" Plugins configuration
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+		"firenvim config
+		let g:firenvim_config = {
+			\ 'globalSettings': {
+				\ 'alt': 'all',
+				\ 'takeover': 'never'
+			\  },
+			\ 'localSettings': {
+				\ '.*': {
+				\ },
+			\ }
+		\ }
+		let fc = g:firenvim_config['localSettings']
+		let fc['https?://.+'] = { 'takeover': 'never', 'priority': 1 }
+		let fc['.*'] = { 'cmdline' : 'neovim' }
+		au BufEnter d8c7fc-e847.cloud.databricks.com_*.txt set filetype=sql
+		au BufEnter mail.google.com_*.txt set filetype=markdown
+		let g:timer_started = v:false
+
+		"vim-slime
+		let g:slime_target = "tmux"
+		let g:slime_paste_file = expand("$HOME/.cache/slime_paste")
+
+		"Vim airline
+		let g:airline#extensions#tabline#enabled = 0 " Enable the list of buffers
+		let g:airline#extensions#tabline#formatter = 'default'  " f/p/file-name.js
+		let g:airline#extensions#tabline#formatter = 'jsformatter' " path-to/f
+		let g:airline#extensions#tabline#formatter = 'unique_tail' " file-name.js
+		let g:airline#extensions#tabline#formatter = 'unique_tail_improved' " f/p/file-name.js
+		let g:airline#extensions#disable_rtp_load=1 "fix error messages when starting neovim
+
+		"Database operations
+		let g:db_ui_save_location = '~/eureciclo/salesops-dw/main-salesops-dw/queries'
+		let g:db_ui_tmp_query_location = '~/eureciclo/salesops-dw/main-salesops-dw/tmp'
+		let g:db_ui_winwidth = 10 " change drawer width
+		let g:db_ui_execute_on_save = 0 " Disable the executing of the query on save
+		let g:db_ui_force_echo_notifications = 1 "Force notifications to be show on command line
+		autocmd FileType sql setlocal omnifunc=vim_dadbod_completion#omni
+]])
