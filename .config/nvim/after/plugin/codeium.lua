fa5d6c7c2933b424d6ec63d3f073ffd025d89766
André Gonzalez <lopescg@gmail.com>
2023-05-07 11:17:39 -0600

    fix codeium keybinds

--- .config/nvim/after/plugin/codeium.lua
+++ .config/nvim/after/plugin/codeium.lua
@@ -1,5 +1,5 @@
 -- Codeium
 vim.g.codeium_disable_bindings = 1
-vim.keymap.set('i', '<C-F>', '<Cmd>call codeium#Accept()<CR>')
-vim.keymap.set('i', '<C-;>', '<Cmd>call codeium#CycleCompletions(1)<CR>')
-vim.keymap.set('i', '<C-\\>', '<Cmd>call codeium#CycleCompletions(-1)<CR>')
+vim.keymap.set('i', '<C-F>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+vim.keymap.set('i', '<C-|>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+vim.keymap.set('i', '<C-\\>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
