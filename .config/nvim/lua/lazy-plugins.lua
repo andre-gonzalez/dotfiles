2aa761d41a39fb455e758d8911744ca268d33f19
André Gonzalez <lopescg@gmail.com>
2023-05-08 18:23:38 -0600

    updated firenvim install on lazy

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -56,7 +56,17 @@ require("lazy").setup({
   "mfussenegger/nvim-dap-python",
   "rcarriga/nvim-dap-ui",
   "theHamsta/nvim-dap-virtual-text",
-  'glacambre/firenvim',
+{
+    'glacambre/firenvim',
+
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    cond = not not vim.g.started_by_firenvim,
+    build = function()
+        require("lazy").load({ plugins = "firenvim", wait = true })
+        vim.fn["firenvim#install"](0)
+    end
+},
   'akinsho/bufferline.nvim',
   {
     'VonHeikemen/lsp-zero.nvim',
