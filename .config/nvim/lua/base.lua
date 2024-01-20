3fd4da4c17ac48e3faec41d2b797ee6cecb1c384
André Gonzalez <lopescg@gmail.com>
2024-01-19 19:43:01 -0600

    change tabwidth to 4 spaces

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -3,8 +3,8 @@
 ------------------------------------------------------------------------------
 
 -- set tab width to 4 spaces
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- set fold to manual
 vim.opt.foldmethod = "manual"
