c6fd7404595cde93eaa6b831ae7227ec2a9076cc
André Gonzalez <lopescg@gmail.com>
2024-06-14 18:29:30 -0600

    fix restore of tmux status line on zen-mode plugin in neovim

--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -1,22 +1,28 @@
 return {
 
-  "folke/zen-mode.nvim",
+	"folke/zen-mode.nvim",
 
 	config = function()
 		vim.keymap.set("n", "<leader>zz", function()
+			require("zen-mode").setup {
+				window = {
+					width = 200,
+					options = {}
+				},
 
-				require("zen-mode").setup {
-						window = {
-							width = 200,
-							options = { }
-						},
-						plugins = {
-							tmux = { enabled = true }, -- disables the tmux statusline
-							},
-				}
+				on_open = function(_)
+					vim.fn.system([[tmux set status off]])
+					vim.fn.system(
+						[[tmux list-panes -F '\#F' | grep -q Z || tmux resize-pane -Z]])
+				end,
+				on_close = function(_)
+					vim.fn.system([[tmux set status on]])
+					vim.fn.system(
+						[[tmux list-panes -F '\#F' | grep -q Z && tmux resize-pane -Z]])
+				end
+			}
 
-				require("zen-mode").toggle()
-
+			require("zen-mode").toggle()
 		end)
 	end
 
