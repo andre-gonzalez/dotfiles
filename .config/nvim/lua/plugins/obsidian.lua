1d670274a8b2845048227d75106639724837e9df
André Gonzalez <lopescg@gmail.com>
2024-07-02 15:29:16 -0600

    Neovim/Add obsidian.nvim plugin

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,82 @@
+return {
+
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	ft = "markdown",
+	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+	-- event = {
+	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+	--   "BufReadPre path/to/my-vault/**.md",
+	--   "BufNewFile path/to/my-vault/**.md",
+	-- },
+
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+
+		-- see below for full list of optional dependencies 👇
+	},
+
+	opts = {
+		workspaces = {
+			{
+				name = "notas",
+				path = "~/projects/notas",
+			},
+		},
+
+		-- see below for full list of options 👇
+	},
+
+	completion = {
+		-- Set to false to disable completion.
+		nvim_cmp = true,
+		-- Trigger completion at 2 chars.
+		min_chars = 2,
+	},
+
+	mappings = {
+		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+		["gf"] = {
+			action = function()
+				return require("obsidian").util.gf_passthrough()
+			end,
+			opts = { noremap = false, expr = true, buffer = true },
+		}
+	},
+
+	new_notes_location = "notes_subdir",
+
+  attachments = {
+    -- The default folder to place images in via `:ObsidianPasteImg`.
+    -- If this is a relative path it will be interpreted as relative to the vault root.
+    -- You can always override this per image by passing a full path to the command instead of just a filename.
+    img_folder = "_attachment" ,  -- This is the default
+    -- A function that determines the text to insert in the note when pasting an image.
+    -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+    -- This is the default implementation.
+    ---@param client obsidian.Client
+    ---@param path obsidian.Path the absolute path to the image file
+    ---@return string
+    img_text_func = function(client, path)
+      path = client:vault_relative_path(path) or path
+      return string.format("![%s](%s)", path.name, path)
+    end,
+  },
+
+  picker = {
+    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+    name = "telescope.nvim",
+    -- Optional, configure key mappings for the picker. These are the defaults.
+    -- Not all pickers support all mappings.
+    mappings = {
+      -- Create a new note from your query.
+      new = "<leader>tx",
+      -- Insert a link to the selected note.
+      insert_link = "<leader>tl",
+    },
+  },
+
+}
