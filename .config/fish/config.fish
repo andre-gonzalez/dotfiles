c788706dc0b5723e96c4b8b305754d0e88015eb8
André Gonzalez <lopescg@gmail.com>
2025-02-04 16:28:05 -0600

    Fish/ Set enviromental variable XDG_DOWNLOAD_DIR

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,7 @@ set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_STATE_HOME "$HOME/.local/state"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_DOWNLOAD_DIR "$HOME/gdrive-pessoal/downloads"
 set -e fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $HOME/.scripts /usr/local/bin /usr/bin $fish_user_paths
 set -gx EDITOR "/usr/bin/nvim"
