5e279a7f834b9a7444a4a46ed67362158e985177
André Gonzalez <lopescg@gmail.com>
2025-05-11 13:33:02 -0600

    Disable plugins that I am not using right now

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,82 +1,124 @@
 return {
 
-	"epwalsh/obsidian.nvim",
-	version = "*", -- recommended, use latest release instead of latest commit
-	lazy = true,
-	ft = "markdown",
-	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-	-- event = {
-	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-	--   "BufReadPre path/to/my-vault/**.md",
-	--   "BufNewFile path/to/my-vault/**.md",
-	-- },
+-- 	"epwalsh/obsidian.nvim",
+-- 	version = "*", -- recommended, use latest release instead of latest commit
+-- 	lazy = true,
+-- 	ft = "markdown",
+-- 	-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+-- 	-- event = {
+-- 	--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+-- 	--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+-- 	--   "BufReadPre path/to/my-vault/**.md",
+-- 	--   "BufNewFile path/to/my-vault/**.md",
+-- 	-- },
 
-	dependencies = {
-		-- Required.
-		"nvim-lua/plenary.nvim",
+-- 	dependencies = {
+-- 		-- Required.
+-- 		"nvim-lua/plenary.nvim",
 
-		-- see below for full list of optional dependencies 👇
-	},
+-- 		-- see below for full list of optional dependencies 👇
+-- 	},
 
-	opts = {
-		workspaces = {
-			{
-				name = "notas",
-				path = "~/projects/notas",
-			},
-		},
+-- 	opts = {
+-- 		workspaces = {
+-- 			{
+-- 				name = "notas",
+-- 				path = "~/projects/notas",
+-- 			},
+-- 		},
 
-		-- see below for full list of options 👇
-	},
+-- 		-- see below for full list of options 👇
+-- 	},
 
-	completion = {
-		-- Set to false to disable completion.
-		nvim_cmp = true,
-		-- Trigger completion at 2 chars.
-		min_chars = 2,
-	},
+-- 	completion = {
+-- 		-- Set to false to disable completion.
+-- 		nvim_cmp = true,
+-- 		-- Trigger completion at 2 chars.
+-- 		min_chars = 2,
+-- 	},
 
-	mappings = {
-		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-		["gf"] = {
-			action = function()
-				return require("obsidian").util.gf_passthrough()
-			end,
-			opts = { noremap = false, expr = true, buffer = true },
-		}
-	},
+-- 	mappings = {
+-- 		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+-- 		["gf"] = {
+-- 			action = function()
+-- 				return require("obsidian").util.gf_passthrough()
+-- 			end,
+-- 			opts = { noremap = false, expr = true, buffer = true },
+-- 		}
+-- 	},
 
-	new_notes_location = "notes_subdir",
+-- 	new_notes_location = "notes_subdir",
 
-  attachments = {
-    -- The default folder to place images in via `:ObsidianPasteImg`.
-    -- If this is a relative path it will be interpreted as relative to the vault root.
-    -- You can always override this per image by passing a full path to the command instead of just a filename.
-    img_folder = "_attachment" ,  -- This is the default
-    -- A function that determines the text to insert in the note when pasting an image.
-    -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
-    -- This is the default implementation.
-    ---@param client obsidian.Client
-    ---@param path obsidian.Path the absolute path to the image file
-    ---@return string
-    img_text_func = function(client, path)
-      path = client:vault_relative_path(path) or path
-      return string.format("![%s](%s)", path.name, path)
-    end,
-  },
+--   attachments = {
+--     -- The default folder to place images in via `:ObsidianPasteImg`.
+--     -- If this is a relative path it will be interpreted as relative to the vault root.
+--     -- You can always override this per image by passing a full path to the command instead of just a filename.
+--     img_folder = "_attachment" ,  -- This is the default
+--     -- A function that determines the text to insert in the note when pasting an image.
+--     -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+--     -- This is the default implementation.
+--     ---@param client obsidian.Client
+--     ---@param path obsidian.Path the absolute path to the image file
+--     ---@return string
+--     img_text_func = function(client, path)
+--       path = client:vault_relative_path(path) or path
+--       return string.format("![%s](%s)", path.name, path)
+--     end,
+--   },
 
-  picker = {
-    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-    name = "telescope.nvim",
-    -- Optional, configure key mappings for the picker. These are the defaults.
-    -- Not all pickers support all mappings.
-    mappings = {
-      -- Create a new note from your query.
-      new = "<leader>tx",
-      -- Insert a link to the selected note.
-      insert_link = "<leader>tl",
-    },
-  },
+--   picker = {
+--     -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+--     name = "telescope.nvim",
+--     -- Optional, configure key mappings for the picker. These are the defaults.
+--     -- Not all pickers support all mappings.
+--     mappings = {
+--       -- Create a new note from your query.
+--       new = "<leader>tx",
+--       -- Insert a link to the selected note.
+--       insert_link = "<leader>tl",
+--     },
+--   },
+--  ui = {
+--     enable = true,  -- set to false to disable all additional syntax features
+--     update_debounce = 200,  -- update delay after a text change (in milliseconds)
+--     max_file_length = 5000,  -- disable UI features for files with more than this many lines
+--     -- Define how various check-boxes are displayed
+--     checkboxes = {
+--       -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+--       [" "] = { char = " ", hl_group = "ObsidianTodo" },
+--       ["x"] = { char = "x", hl_group = "ObsidianDone" },
+--       [">"] = { char = ">", hl_group = "ObsidianRightArrow" },
+--       ["~"] = { char = "~", hl_group = "ObsidianTilde" },
+--       ["!"] = { char = "!", hl_group = "ObsidianImportant" },
+--       -- Replace the above with this if you don't have a patched font:
+--       -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+--       -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+--       -- You can also add more custom ones...
+--     },
+--     -- Use bullet marks for non-checkbox lists.
+--     bullets = { char = "•", hl_group = "ObsidianBullet" },
+--     external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+--     -- Replace the above with this if you don't have a patched font:
+--     -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+--     reference_text = { hl_group = "ObsidianRefText" },
+--     highlight_text = { hl_group = "ObsidianHighlightText" },
+--     tags = { hl_group = "ObsidianTag" },
+--     block_ids = { hl_group = "ObsidianBlockID" },
+--     hl_groups = {
+--       -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+--       ObsidianTodo = { bold = true, fg = "#f78c6c" },
+--       ObsidianDone = { bold = true, fg = "#89ddff" },
+--       ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+--       ObsidianTilde = { bold = true, fg = "#ff5370" },
+--       ObsidianImportant = { bold = true, fg = "#d73128" },
+--       ObsidianBullet = { bold = true, fg = "#89ddff" },
+--       ObsidianRefText = { underline = true, fg = "#c792ea" },
+--       ObsidianExtLinkIcon = { fg = "#c792ea" },
+--       ObsidianTag = { italic = true, fg = "#89ddff" },
+--       ObsidianBlockID = { italic = true, fg = "#89ddff" },
+--       ObsidianHighlightText = { bg = "#75662e" },
+--     },
+--   },
 
 }
--- .config/nvim/lua/plugins/vim-dadbod.lua
+++ .config/nvim/lua/plugins/vim-dadbod.lua
@@ -1,11 +1,28 @@
 return {
-	"tpope/vim-dadbod",
+	-- 'kristijanhusak/vim-dadbod-ui',
+	-- dependencies = {
+	-- 	{ 'tpope/vim-dadbod',                     lazy = true },
+	-- 	{ 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+	-- },
+	-- cmd = {
+	-- 	'DBUI',
+	-- 	'DBUIToggle',
+	-- 	'DBUIAddConnection',
+	-- 	'DBUIFindBuffer',
+	-- },
 
-	"kristijanhusak/vim-dadbod-completion",
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		config = function()
-			vim.keymap.set("n", "<leader>bf", ":DBUIFindBuffer<CR>")
-		end,
-	},
+	-- config = function()
+	-- 	vim.keymap.set("n", "<leader>bf", ":DBUIFindBuffer<CR>")
+
+
+	-- 	vim.g.dbs = {
+	-- 		{ name = 'postgres', url = 'postgres://postgres:postgres@localhost:5432/postgres' },
+	-- 	}
+	-- end,
+	-- init = function()
+	-- 	-- Your DBUI configuration
+	-- 	vim.g.db_ui_use_nerd_fonts = 1
+	-- end,
+
+
 }
