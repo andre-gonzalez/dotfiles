701e132c434e8eb36de6a247e07c8029f4572a00
André Gonzalez <lopescg@gmail.com>
2025-05-11 13:31:43 -0600

    Ident line/ change ident line plugin from Yggdroot/identLine to lukas-reineke/ident-blankline.nvim

--- .config/nvim/lua/plugins/indent-line.lua
+++ .config/nvim/lua/plugins/indent-line.lua
@@ -1,3 +1,16 @@
 return {
-	"Yggdroot/indentLine",
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl", -- use ibl module as per v3
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    indent = {
+      char = "│",
+    },
+    scope = {
+      enabled = false, -- disables underline/highlighting of functions, blocks, etc.
+    },
+    whitespace = {
+      remove_blankline_trail = true,
+    },
+  },
 }
