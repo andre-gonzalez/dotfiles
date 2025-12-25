ac07b346fe838ee5d8ea846e539d46afce531b30
André Gonzalez <lopescg@gmail.com>
2025-12-25 08:27:58 -0300

    Nvim/ remove the enconding and the penguim logo from appearing in the bar

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -36,6 +36,27 @@ return {
 						c = { fg = colors.fg, bg = colors.bg },
 					},
 				}
+			},
+			sections = {
+				lualine_a = {
+					{
+						'mode',
+						icons_enabled = false,
+					}
+				},
+				lualine_b = { 'branch', 'diff', 'diagnostics' },
+				lualine_c = { 'filename' },
+				lualine_x = { 'filetype' },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' }
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { 'filename' },
+				lualine_x = { 'location' },
+				lualine_y = {},
+				lualine_z = {}
 			}
 		})
 	end
