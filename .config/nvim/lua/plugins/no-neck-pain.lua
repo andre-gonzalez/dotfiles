b4d818fdd3fc81ca56ab85ccbc95beb7f644361f
André Gonzalez <lopescg@gmail.com>
2025-12-27 11:36:21 -0300

    Nvim/ set plugin no-neck-pain to only load when the filetype is newsboat so it does not start with every neovim session

--- .config/nvim/lua/plugins/no-neck-pain.lua
+++ .config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,5 +1,6 @@
 return {
   "shortcuts/no-neck-pain.nvim",
+  ft = "newsboat", -- Lazy load only for newsboat filetype
   opts = {
     width = 100, -- or whatever feels good for your terminal width
   },
