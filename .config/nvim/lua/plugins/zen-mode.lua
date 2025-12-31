693495bc8c1dc898de53ae070ab89897ca7afad5
André Gonzalez <lopescg@gmail.com>
2025-12-31 10:16:06 -0300

    Nvim/ removed the colorcolumn from appearing when zen mode is enabled

--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -21,10 +21,12 @@ return {
         gitsigns = { enabled = false },
       },
       on_open = function()
+        vim.opt.colorcolumn = ""
         vim.fn.system([[tmux set status off]])
         vim.fn.system([[tmux list-panes -F '\#F' | grep -q Z || tmux resize-pane -Z]])
       end,
       on_close = function()
+        vim.opt.colorcolumn = "79"
         vim.fn.system([[tmux set status on]])
         vim.fn.system([[tmux list-panes -F '\#F' | grep -q Z && tmux resize-pane -Z]])
       end,
