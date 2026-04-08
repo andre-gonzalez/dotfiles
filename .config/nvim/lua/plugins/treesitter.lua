5124ce3d45473d70c792bb263f81c20a47b65696
André Gonzalez <lopescg@gmail.com>
2026-04-08 20:55:02 -0300

    Neovim/ update treesitter config to the latest treesitter version

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,56 +1,24 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" }, -- Lazy load when a file is opened
 	config = function()
-		local config = require('nvim-treesitter.configs')
-		config.setup({
-			auto_install = true,
-			ensure_installed = {
-				"bash", "c", "cpp", "diff", "git_config", "gitcommit", "gitignore", "json",
-				"lua", "markdown", "markdown_inline", "nix", "python", "query", "regex",
-				"toml", "vim", "vimdoc", "yaml"
-			},
-			highlight = { enable = true },
-			indent = { enable = true },
+		require("nvim-treesitter").setup()
 
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<CR>",
-					node_incremental = "<CR>",
-					scope_incremental = "<TAB>",
-					node_decremental = "<BS>",
-				},
-			},
+		require("nvim-treesitter").install({
+			"bash", "c", "cpp", "diff", "git_config", "gitcommit", "gitignore", "json",
+			"lua", "markdown", "markdown_inline", "nix", "python", "query", "regex",
+			"toml", "vim", "vimdoc", "yaml"
+		})
 
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-						["ap"] = "@parameter.outer",
-						["ip"] = "@parameter.inner",
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-					},
-				},
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("treesitter_features", { clear = true }),
+			pattern = "*",
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end
-
 }
