769349f554b0ea496440f5c97c2ae348abaebd3b
André Gonzalez <lopescg@gmail.com>
2026-08-09 16:06:30 -0300

    Neovim/ disable DBUI notifications

--- .config/nvim/lua/plugins/vim-dadbod.lua
+++ .config/nvim/lua/plugins/vim-dadbod.lua
@@ -55,6 +55,7 @@ return {
 		},
 		config = function()
 			vim.g.db_ui_use_nerd_fonts = 1
+			vim.g.db_ui_disable_info_notifications = 1
 			vim.g.db_ui_save_location = vim.fn.stdpath("data") .. "/db_ui"
 
 			-- Credentials are stored in lua/secrets.lua (gitignored).
